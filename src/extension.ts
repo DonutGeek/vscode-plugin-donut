56f33fcd17409908b92b62ec84608603d676edc6
DonutGeek <13223057509@163.com>
2025-05-22 18:18:50 +0800

    refactor: 移除未使用的helloWorld命令并更新package.json
    移除extension.ts中的helloWorld命令，因为它未被使用。同时，更新package.json以添加关键词、发布者信息，并移除与helloWorld命令相关的配置。这些更改旨在简化代码并增强插件的元数据。

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,6 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "vscode-plugin-donut" is now active!');
 
-	const disposable = vscode.commands.registerCommand('vscode-plugin-donut.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-plugin-donut!');
-	});
-	context.subscriptions.push(disposable);
-
 	const copyNameCmd = vscode.commands.registerCommand('vscode-plugin-donut.copyName', (uri) => {
 		if (!uri || !uri.fsPath) return;
 
